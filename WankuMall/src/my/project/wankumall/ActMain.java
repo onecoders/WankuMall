d8bf2bef9237036284df7e945a1a01613e39c79b
onecoders <onecoders@gmail.com>
2014-07-10 22:44:09 +0800

    init Wanku Mall project

---
+++ WankuMall/src/my/project/wankumall/ActMain.java
@@ -0,0 +1,22 @@
+package my.project.wankumall;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ActMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.act_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.act_main, menu);
+		return true;
+	}
+
+}
